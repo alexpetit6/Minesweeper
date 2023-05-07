ac00e2a169db0b11f0c2f5bcc18606f97787ebf4
Alex Petit <alexpetit6@gmail.com>
2023-05-06 19:16:27 -0700

    difficulty buttons change size of grid and add divs to board

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 /*----- constants -----*/
 const boardSizes = {
-    e: [9, 9],
-    m: [16, 16],
-    h: [16, 30]
+    // first element is rows, second is columns, third is mineCount
+    e: [9, 9, 10],
+    m: [16, 16, 40],
+    h: [16, 30, 99]
 }
 class mainCell {
     constructor(isRevealed, isFlagged, hasMine, minesTouching) {
@@ -12,11 +13,14 @@ class mainCell {
         this.minesTouching = minesTouching;
     }
 }
+
+
 /*----- state variables -----*/
 
 const cell = new mainCell(false, false, false, 0)
 let board = []
 let boardSize;
+let mineCount;
 
   /*----- cached elements  -----*/
 const boardEl = document.getElementById('board')
@@ -59,6 +63,13 @@ document.querySelector('header').addEventListener('click', handleDifficulty)
 
   function renderBoard() {
     renderBoardSize();
+    boardEl.style.gridTemplateRows = `repeat(${boardSizes[boardSize][0]}, 5vmin)`;
+    boardEl.style.gridTemplateColumns = `repeat(${boardSizes[boardSize][1]}, 5vmin)`;
+    let boardTotal = (boardSizes[boardSize][0]) * (boardSizes[boardSize][1]);
+    for (let i = 0; i < boardTotal; i++) {
+       const newCellEl = document.createElement('div');
+       boardEl.append(newCellEl);
+    }
   }
 
   function renderControls() {
@@ -75,7 +86,7 @@ document.querySelector('header').addEventListener('click', handleDifficulty)
 
   function renderBoardSize() {
     board.length = 0;
-    for (let i = 0; i < boardSizes[boardSize][0]; i++) {
+    for (let i = 0; i < boardSizes[boardSize][0]; i++) {''
         board.push([])
     }
     board.forEach(function(arr) {
@@ -92,5 +103,10 @@ document.querySelector('header').addEventListener('click', handleDifficulty)
     if (evt.target.tagName !== 'BUTTON') return;
     //------------------------------------------
     boardSize = evt.target.getAttribute('id');
+    // removes all divs in boardEl
+    while (boardEl.firstChild) {
+        boardEl.removeChild(boardEl.firstChild)
+    }
+    //------------------------------------------
     render();
   }
